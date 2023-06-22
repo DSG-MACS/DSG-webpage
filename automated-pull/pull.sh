bd9fcae017a6746f6f051da8bb34f5611734d053
root <me@akilan.io>
2023-06-22 15:06:53 +0000

    added new way to copy files

--- automated-pull/pull.sh
+++ automated-pull/pull.sh
@@ -5,7 +5,7 @@ do
         git pull origin main
         cd webpage/
         hugo 
-        cp -a public/ /var/www/html/
+        rsync -a public/ /var/www/html
         cd ..
         echo "Press [CTRL+C] to stop.."
 done
