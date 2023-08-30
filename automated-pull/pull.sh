7b2b0e37a6c50150a2db11609cf75108e31c3718
root <me@akilan.io>
2023-08-30 17:10:16 +0000

    added changes for the remote and mirror

--- automated-pull/pull.sh
+++ automated-pull/pull.sh
@@ -2,10 +2,11 @@ cd ..
 while :
 do
 	sleep 5
-        git pull origin main
+        git pull newgitlab main
         cd webpage/
         hugo 
         rsync -a public/ /var/www/html
         cd ..
+        git push github main
         echo "Press [CTRL+C] to stop.."
 done
