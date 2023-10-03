4bc6a1ae3f246ce3e17f4d8d554d55ecd2a529b2
Akilan <me@akilan.io>
2023-10-03 10:35:22 +0100

    added server to poll changes

--- automated-pull/MACSserver.sh
+++ automated-pull/MACSserver.sh
@@ -3,9 +3,10 @@ export PATH=/home/as251:${PATH}
 while :
 do
 	sleep 5
-        git pull newgitlab main
+        git pull origin main
         cd webpage/
         hugo 
         rsync -a public/ /home/as251/www/public
         echo "Press [CTRL+C] to stop.."
+        cd ..
 done
