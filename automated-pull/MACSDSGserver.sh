4e4bb09cb1193a4394431ee97931e66acd6c01a1
Akilan <me@akilan.io>
2025-01-27 13:07:27 +0000

    removed unwanted files

--- automated-pull/MACSDSGserver.sh
+++ automated-pull/MACSDSGserver.sh
@@ -9,7 +9,7 @@ do
         rsync -a public/ /home/dsg/www/public
         chmod 775 -R /home/dsg/www/public/
         # chmod a+r -r /home/dsg/www/public/*
-        echo "Press [CTRL+C] to stop.."
+        # echo "Press [CTRL+C] to stop.."
         cd ..
         git push github-test main
         git push gitlab-fix main
